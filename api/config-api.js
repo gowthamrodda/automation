6384884cce5ea191315bf6bbb6bced30107a4b94
Bhaskar Reddy Yasani <bhaskarreddy.yasani@gmail.com>
2017-08-18 11:47:43 -0700

    my npm changes

--- api/config-api.js
+++ api/config-api.js
@@ -3,36 +3,46 @@ let request = require('request');
 const express = require('express');
 const router = express.Router();
 
-router.get('/config', getConfig);
+router.post('/config', getConfig);
 
 module.exports = router;
 
 
 function getConfig(req, res) {
-    console.log('IN Get config');
-    let URL = 'http://10.202.10.46:3000/rpc/get-lacp-interface-information?interface-name=ae101';
+    console.log('In Get config');
+
+    let customHost;
+    let customPath;
+    let customBody;
+    if (req.body) {
+      customHost = req.body.host;
+      customPath = req.body.path;
+      customBody = req.body.query;
+    }
+    console.log(req.body);
+  //  let URL = 'http://10.202.10.46:3000/rpc/get-lacp-interface-information?interface-name=ae101';
+    let URL = `${customHost}/${customPath}?${customBody}`;
+
     const headers = {
         Authorization: 'Basic cm9vdDpzaXRsYWIxMjMh',
-        Accept: 'Application/json',
-        ContentType: 'plain/text'
+        Accept: 'application/json',
+        ContenType: 'application/xml'
     };
     let options = {
         url: URL,
         headers: headers
     };
-
-    request(options, (error, response, body) => {
+    console.log('trace 1');
+        request(options, (error, response, body) => {
         if (error) {
             return res
                 .status(500)
                 .json({error: error})
         }
-        let info = JSON.parse(body);
-        console.log(info);
+        console.log(body);
+        return res
+                .status(response.statusCode)
+                .send(body);
     });
 
 }
-
-
-
-

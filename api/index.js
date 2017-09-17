2baf25667358827c9f5ec14ca8a10fb0b8809c37
Gowtham Kumar <gowtham@olivetech.net>
2017-09-17 10:57:18 +0530

    Adds user api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ let router = require('express').Router();
 router.use(require('./testbed-api'));
 router.use(require('./device-api'));
 router.use(require('./config-api'));
+router.use(require('./user-api'));
 
 
 module.exports = router;
